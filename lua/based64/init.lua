97fcd551eaae6a082bb089731f3898eb62b3b100
THBABUA <thangesh.babu_a@mercedes-benz.com>
2025-01-09 14:04:18 +0800

    updated

--- lua/based64/init.lua
+++ lua/based64/init.lua
@@ -28,44 +28,40 @@ end
 
 
 local function executer(action)
-    local start_row = vim.fn.line "v"
-    local start_col = vim.fn.col "v"
-    local _, end_row, end_col = unpack(vim.fn.getcurpos())
 
-    if start_row ~= end_row then 
-        vim.notify("Multiline Visual Selection is not supported!", vim.log.levels.TRACE)
-        return nil
-    end
+       local start_row = vim.fn.line "v"
+       local start_col = vim.fn.col "v"
+       local _, end_row, end_col = unpack(vim.fn.getcurpos())
 
-    -- Swap start and end if start is greater than end
-    if start_col > end_col then 
-        tmp = start_col
-        start_col = end_col 
-        end_col = tmp
-    end
-
-    local selected_text = get_visually_selected_string(start_row, start_col, end_row, end_col)
-    print("Selected text: ", selected_text)  -- Debugging: Show the selected text
+         if start_row ~= end_row then 
+            vim.notify("Multiline Visual Selection is not supported!", vim.log.levels.TRACE)
+            return nil
+        end
     
-    local result = base64(selected_text, action)
+        -- swap start and end if start is greater than end
+        if start_col > end_col then 
+            tmp=start_col
+            start_col=end_col end_col=tmp
+        end
+        
+        local selected_text=  get_visually_selected_string(start_row, start_col, end_row, end_col)
 
-    if not result then return end
+        local result = base64(selected_text,action)
 
-    print("Result: ", result)  -- Debugging: Show the result (encoded/decoded string)
+        local ok,res = pcall(vim.api.nvim_buf_set_text,0, start_row-1, start_col-1, end_row-1, end_col, {result})
+        print(res,ok)
+        if not ok then
+           vim.api.nvim_buf_set_text(0, start_row-1, start_col-1, end_row-1, end_col-1, {result})
+        end
 
-    local ok, res = pcall(vim.api.nvim_buf_set_text, 0, start_row-1, start_col-1, end_row-1, end_col, {result})
-    print(res, ok)
-    if not ok then
-        vim.api.nvim_buf_set_text(0, start_row-1, start_col-1, end_row-1, end_col-1, {result})
-    end
+       -- Move the cursor to the start of the selection 
+       vim.api.nvim_win_set_cursor(0, {start_row, start_col-1})
+      
+       -- -- go to normal mode
+        vim.api.nvim_input('<Esc>')
 
-    -- Move cursor to the start of the selection
-    vim.api.nvim_win_set_cursor(0, {start_row, math.min(start_col - 1, #result)})
-
-    -- Return to normal mode
-    vim.api.nvim_input('<Esc>')
+       -- hello hello hello
 end
-
 
 function M.encode()
     executer("encode")
